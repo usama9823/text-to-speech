36d174c58c7d8b71e77ac2a33947b9066acc9881
Usama Shehroz <usama9823@gmail.com>
2024-02-25 13:37:00 +0100

    Replacing brackets

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import pyttsx3
 if __name__ == '__main__':
     engine = pyttsx3.init()
     speech = input("please enter your text to say: ")
-    while(True):
+    while True:
         engine.say(speech)
         engine.runAndWait()
         speech = input("please enter quit to quit or enter another text to say: ")
-        if(speech=='quit'):
+        if speech=='quit':
             engine.say('Thankyou for using text to speech')
             engine.runAndWait()
             break
